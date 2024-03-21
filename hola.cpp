eed1fc08113fbcc1cb6304c5a4b8426cd8729830
noeher <135664476+noeher@users.noreply.github.com>
2024-03-21 19:32:04 +0100

    Update hola.cpp

--- hola.cpp
+++ hola.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main()
 {
 	cout << "1. Hola mundo" << endl;
+	cout << "2. Adiós mundo" << endl;
 	//system("pause"); Esto no funciona en linux.
 	//system("clear");
 	cout << "Fin" << endl;
