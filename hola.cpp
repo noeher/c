c83e4142cf2cb9205bd7658c33a2a49cefeef65c
noeher <135664476+noeher@users.noreply.github.com>
2024-03-21 19:20:06 +0100

    Add files via upload

---
+++ hola.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+	cout << "1. Hola mundo" << endl;
+	//system("pause"); Esto no funciona en linux.
+	//system("clear");
+	cout << "Fin" << endl;
+}
